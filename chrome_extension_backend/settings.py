f732c8d886b4f94547ff872f885a4d3fd6d944da
Precious <okolaaprecious@gmail.com>
2023-09-29 22:51:40 +0100

    fixed bugs

--- chrome_extension_backend/settings.py
+++ chrome_extension_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,3 +120,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Media URL
 MEDIA_URL = '/media/'
+
+# Media Root
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
